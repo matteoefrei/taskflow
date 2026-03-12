85fea219011dbe7b820f807095f157ad7a6c8763
matteoefrei <matteo.moitrel@efrei.net>
2026-03-12 12:01:19 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -16,7 +16,7 @@ def client():
 def clean_tasks():
     """Nettoyer les tâches entre chaque test"""
     from app import tasks
-    tasks.clear()
+    tasks.cleacczr()
     yield
     tasks.clear()
 
